61e92c4881af7f703929d260e583dc8943d0b9ac
R_v_kybe <renat_romanov_03@mail.ru>
2026-06-02 13:30:14 +0300

    fix(): путь до шрифта

--- font/makefont/makefont.php
+++ font/makefont/makefont.php
@@ -268,9 +268,6 @@ function CheckTTF($file) {
  * type:     font type if fontfile is empty                                     *
  *******************************************************************************/
 function MakeFont($fontfile, $afmfile, $enc = 'cp1252', $patch = array(), $type = 'TrueType') {
-    //Generate a font definition file
-    if (get_magic_quotes_runtime())
-        @set_magic_quotes_runtime(0);
     ini_set('auto_detect_line_endings', '1');
     if ($enc) {
         $map = ReadMap($enc);
